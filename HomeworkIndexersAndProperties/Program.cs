78a131e1cd458ca6e522f9100928375b2fcdfefb
Anuar <beisekeyev.a.1992.ast@gmail.com>
2021-01-13 19:17:11 +0600

    добавил репозиторий по домашнему заданию на тему индексаторы и свойства

---
+++ HomeworkIndexersAndProperties/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HomeworkIndexersAndProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();            
+            bool isItInt; 
+            int start = 0, end = 1;           
+
+            do
+            {
+                Console.Write("Введите начало диапазона: ");
+                isItInt = int.TryParse(Console.ReadLine(), out start);
+                if (!isItInt)
+                {
+                    Console.WriteLine("Не верный ввод.");
+                }
+
+            } while (!isItInt);
+
+            do
+            {
+                Console.Write("Введите конец диапазона: ");
+                isItInt = int.TryParse(Console.ReadLine(), out end);
+
+                if (!isItInt || start > end)
+                {
+                    Console.WriteLine("Не верный ввод.");
+                }
+
+            } while (!isItInt || start > end);
+
+            RangeOfArray array = new RangeOfArray(start, end);
+
+            for (int i = start; i <= end; i++)
+            {
+                array[i] = random.Next(0, 100);
+                Console.WriteLine($"Массив целых чисел[{i}] = {array[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeworkIndexersAndProperties/RangeOfArray.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkIndexersAndProperties
+{
+    public class RangeOfArray
+    {
+        private int[] _array;
+        private int _size;
+        private int _start;
+        private int _end;
+        public RangeOfArray()
+        {
+
+        }
+        public RangeOfArray(int size)
+        {
+
+            Size = size;
+            Start = 0;
+            End = Size - 1;
+            _array = new int[Size];
+        }
+        public RangeOfArray(int start ,int end)
+        {
+            Start = start;
+            End = end;
+            Size = end - start + 1;        
+            _array = new int[Size];
+        }
+
+        public int Start
+        {
+            get { return _start; }
+            set
+            {
+                if(value > End)
+                {
+                    _start = value;
+                }
+            }
+        }
+        public int End
+        {
+            get { return _end; }
+            set
+            {
+                if(value < Start)
+                {
+                    _end = value;
+                }
+            }
+        }
+        public int Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return _array[Math.Abs(i) - Math.Abs(Start)];
+            }
+            set
+            {
+                _array[Math.Abs(i) - Math.Abs(Start)] = value;
+            }
+        }
+
+        
+
+       
+
+
+    }
+}
